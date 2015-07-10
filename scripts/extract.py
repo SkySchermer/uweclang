dfb4ddb259dee84ffc40702afb5a4ba07bfcda97
Skylor R Schermer <skyschermer@gmail.com>
2015-07-09 22:09:01 -0500

    FEATURE: Completed working extract script.

--- scripts/extract.py
+++ scripts/extract.py
@@ -12,10 +12,9 @@ def extract_plaintext_from_docx(input_filename, output_filename, verbosity=1):
     xml = uweclang.get_document_xml(input_filename, verbosity=verbosity)
     doc = uweclang.xml_to_plain(xml, verbosity=verbosity)
 
-
     # Open file for writing.
     if verbosity >= 2:
-        print('Opening file', os.path.basename(output_filename))
+        print('  Opening file', os.path.basename(output_filename))
 
     try:
         out = open(output_filename, 'w')
@@ -28,11 +27,10 @@ def extract_plaintext_from_docx(input_filename, output_filename, verbosity=1):
 
     # Write to file.
     if verbosity >= 2:
-        print('Writing to file',os.path.basename(output_filename))
+        print('  Writing to file',os.path.basename(output_filename))
 
     try:
-        pass
-        # out.write(doc.strip())
+        out.write(doc.strip())
     except IOError as e:
         print('Could not write to file {}: {}'.format(os.path.basename(output_filename),
                                                       e))
@@ -41,20 +39,21 @@ def extract_plaintext_from_docx(input_filename, output_filename, verbosity=1):
     out.close()
 
     if verbosity >= 1:
-        print('Extracted plaintext from {} to {}'.format(os.path.basename(input_filename),
-                                                         os.path.basename(output_filename)))
+        print('Extracted plaintext from '
+              '{} into {}'.format(os.path.basename(input_filename),
+                                  os.path.basename(output_filename)))
 
 
 if __name__ == '__main__':
 
+    # Add extra command line arguments for this script.
     parser = argparse.ArgumentParser(
         description='Extracts plaintext from docx files',
         parents=[uweclang.BATCH_PARSER])
 
-
     parser.add_argument('-e', '--extensions',
                         nargs='*',
-                        default='.docx',
+                        default=['.docx'],
                         metavar='ext',
                         dest='ext',
                         help='accepted file extensions for input')
@@ -65,29 +64,27 @@ if __name__ == '__main__':
                         metavar='oext',
                         dest='oext',
                         help='extension for output file')
-    # Example args:
-    # batch=10, ext='.docx', file='.', file_extra=None, output='.',
-    # quiet=False, verbose=None
 
+    # Parse command line arguments.
     args = parser.parse_args()
 
-    # Comnine extra '-f' files with position file arguments.
-    paths = list(args.file)
-    if args.extra_files is not None:
-        paths.extend(args.extra_files)
 
+    # Set default verbosity.
     if args.verbose is None:
         args.verbose = 1
 
-    # Resolve absolute paths.
-    if args.verbose >= 3:
-        print('Converting input paths from relative to absolute...')
 
-    out_path = os.path.abspath(args.output)
+    # Comnine extra '-f' files with position file arguments.
+    paths = args.file
+    if args.extra_files is not None:
+        paths.extend(args.extra_files)
 
 
+    # Setup extension checking function
+    args.ext = args.ext
     def is_valid_target(file):
-        return os.path.splitext(file)[1] == args.ext
+        return os.path.splitext(file)[1] in args.ext
+
 
     # Identify all the files to process.
     files = []
@@ -95,39 +92,65 @@ if __name__ == '__main__':
     for path in paths:
         if os.path.isdir(path):
             # Handle a directory.
+            if args.verbose >= 3:
+                print('Finding files in', path)
 
-            # The os.walk call will traverse the directory. x[2] Pulls out the
-            # list of files. The chain.from_iterable will flatten the list of
-            # lists.
-            targets = chain.from_iterable([x[2] for x in os.walk(path)])
+            # The os.walk call will traverse the directory producing
+            # (path, dir, file) tuples. The lambda joins the path to the file,
+            # and chain.from_iterable will flatten the list of lists.
+            targets = []
+            for x in os.walk(path):
+                targets.append(list(map(lambda l: os.path.join(x[0], l), x[2])))
+            targets = chain.from_iterable(targets)
 
             for x in targets:
+                if args.verbose >= 3:
+                    print('  Found file', path)
+
                 # Select list based on is_valid_target, and append to it.
                 # This is a bit clever...
                 (ignored_files, files)[is_valid_target(x)].append(x)
 
         else:
+            if args.verbose >= 3:
+                print('Found file', path)
+
             # Handle a file.
             (ignored_files, files)[is_valid_target(path)].append(path)
 
+
     # Resolve absolute paths.
+    if args.verbose >= 3:
+        print('Resolving absolute paths...')
+
     files = map(os.path.abspath, files)
+    out_path = os.path.abspath(args.output)
+
 
     # Print list of ignored files.
     if args.verbose >= 1:
         for x in ignored_files:
             print("Ignore file:", x)
+        print('--- Begin Processing ---')
 
-    # Normal Process:
-    for filename in files:
-        name_part = os.path.splitext(path)[0]
-        extract_plaintext_from_docx(filename,
-                                    os.path.join(out_path, name_part + args.oext),
-                                    verbosity=args.verbose)
 
-    # # Batch process:
-    # uweclang.batch_process(extract_plaintext_from_docx,
-    #                            in_files=batch_directories,
-    #                            out_dir=out_path,
-    #                            batch_size=args.batch,
-    #                            verbosity=args.verbose)
+    # Process the files.
+    if args.batch_mode == 'none':
+        # Process each file seperately.
+        for filename in files:
+            name_part = os.path.splitext(os.path.basename(filename))[0]
+            extract_plaintext_from_docx(filename,
+                                        os.path.join(out_path,
+                                                     name_part + args.oext),
+                                        verbosity=args.verbose)
+    else:
+        # Process files in batch mode.
+        uweclang.batch_process(extract_plaintext_from_docx,
+                               in_files=batch_directories,
+                               out_dir=out_path,
+                               batch_size=args.batch_size,
+                               batch_mode=args.batch_mode,
+                               verbosity=args.verbose)
+
+    if args.verbose >= 1:
+        print('--- End Processing ---')
