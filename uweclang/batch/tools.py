af24df27051d0222c260e1f7c80ac45f70540c49
Skylor R Schermer <skyschermer@gmail.com>
2015-07-14 00:27:54 -0500

    FIX: Re-added unicode_literals...

--- uweclang/batch/tools.py
+++ uweclang/batch/tools.py
@@ -24,6 +24,7 @@ Module Globals:
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+from __future__ import unicode_literals
 
 # Standard imports
 import os
--- uweclang/plain/tools.py
+++ uweclang/plain/tools.py
@@ -6,6 +6,7 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+from __future__ import unicode_literals
 
 
 def line_split(text, remove_empty_lines=True, strip_lines=True, sep='\n'):
